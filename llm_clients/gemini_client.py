e0c06c88d56f59ff3c0eac1b273552ebb7df3069
purnima shrivastava <purnimashrivastava05@gmail.com>
2026-02-05 17:03:40 +0530

    Update gemini_client.py

--- llm_clients/gemini_client.py
+++ llm_clients/gemini_client.py
@@ -1,1 +1,11 @@
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+def ask_gemini(prompt, model="models/gemini-1.5-flash"):
+    model = genai.GenerativeModel(model)
+    response = model.generate_content(prompt)
+    return response.text
 
